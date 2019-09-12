30e6759532ea54d00a536c45e19870a21b6c43c2
Naeem Baghi <naeembaghi1992@gmail.com>
2019-09-12 13:58:59 +0430

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-rm "$HOME/.config/nvim/init.vim"
-ln "$PWD/nvim" "$HOME/.config/nvim"
+rm -rf "$HOME/.config/nvim"
+ln -sf "$PWD/nvim" "$HOME/.config/nvim"
 
 # Install vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -sf "$PWD/git/config" "$HOME/.gitconfig"
+ln -sf "$PWD/git/.gitconfig" "$HOME/.gitconfig"
