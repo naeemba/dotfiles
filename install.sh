70594f7fc2071c82926e672da8aae8adc04dc8f4
Naeem Baghi <naeembaghi1992@gmail.com>
2019-09-12 11:35:31 +0430

    add nvim config

---
+++ install.sh
@@ -0,0 +1,6 @@
+rm "$HOME/.config/nvim/init.vim"
+ln "$PWD/nvim" "$HOME/.config/nvim"
+
+# Install vim plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
