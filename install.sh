cf492e4641e090018564642e41ff9af7cc0a60e2
Naeem Baghi <naeembaghi1992@gmail.com>
2019-09-12 13:31:34 +0430

    add git to install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln "$PWD/nvim" "$HOME/.config/nvim"
 # Install vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -sf "$PWD/git/config" "$HOME/.gitconfig"
