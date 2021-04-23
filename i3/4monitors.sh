a570f62eef0ff0c03f89e92563f084ed94e3380a
Naeem Baghi <baghinaeem@gmail.com>
2021-04-24 00:01:17 +0430

    update monitors config

--- i3/4monitors.sh
+++ i3/4monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1.2 --primary --mode 2560x1440 --pos 1920x0 --rotate left --output DP-1.3 --mode 1280x1024 --pos 5280x1608 --rotate normal --output DP-0 --mode 1920x1080 --pos 3360x903 --rotate normal --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1080 --pos 0x903 --rotate normal
+xrandr --output DP-1.1 --mode 2560x1440 --pos 1920x0 --rotate left --output DP-1.3 --mode 1280x1024 --pos 5280x959 --rotate normal --output DP-0 --mode 1920x1080 --pos 3360x903 --rotate normal --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --mode 1920x1080 --pos 0x903 --rotate normal
