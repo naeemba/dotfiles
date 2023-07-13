c038dad3470fc79ef91527c5e9c2d070ae7da98d
Naeem Baghi <baghinaeem@gmail.com>
2023-07-13 14:29:18 +0330

    update i3 and polybar configs

--- i3/unsplash.sh
+++ i3/unsplash.sh
@@ -3,33 +3,34 @@
 # Created by Collin McKinley (collinm.xyz)
 
 # Settings
-RUN_AS_DAEMON=0                           # Run unspla.sh as a daemon if 1, if 0 unspla.sh will set the image then exit
-BACKGROUND_RES="5560x1920"                # Set the size of unsplash images to pull
-PARAMETERS="fruit"                  # Set any other parametes to pull in images. add a comma between parameters ( nature, backgrounds)
-BACKGROUND_COMMAND="feh --bg-fill --no-xinerama"       # The command that will be used to set the background, default is feh.  Image will be supplied after the command.
-IMAGE_SAVE_DIR="/tmp/unsplash/photo.jpg"  # Directory to save the images to
-IMAGE_FORMAT=".jpg"
-IMAGE_DIR="/tmp/unsplash/"
+# RUN_AS_DAEMON=0                           # Run unspla.sh as a daemon if 1, if 0 unspla.sh will set the image then exit
+# BACKGROUND_RES="5560x1920"                # Set the size of unsplash images to pull
+# PARAMETERS="fruit"                  # Set any other parametes to pull in images. add a comma between parameters ( nature, backgrounds)
+# BACKGROUND_COMMAND="feh --bg-fill --no-xinerama"       # The command that will be used to set the background, default is feh.  Image will be supplied after the command.
+# IMAGE_SAVE_DIR="/tmp/unsplash/photo.jpg"  # Directory to save the images to
+# IMAGE_FORMAT=".jpg"
+# IMAGE_DIR="/tmp/unsplash/"
 
 #Do not change unless you need to
-UNSPLASH_URL="https://source.unsplash.com/random/$BACKGROUND_RES/?$PARAMETERS"
+# UNSPLASH_URL="https://source.unsplash.com/random/$BACKGROUND_RES/?$PARAMETERS"
 # https://source.unsplash.com/random/2560x1440/?fruit
 
 # Daemon Settings (only modify if you are using unspla.sh as a daemon)
-UPDATE_INTERVAL=86400                     # How long to wait before changing the picture (seconds)
+# UPDATE_INTERVAL=86400                     # How long to wait before changing the picture (seconds)
 
 
-mkdir -p /tmp/unsplash
-MONITORS=$(polybar --list-monitors | cut -d":" -f1)
+# mkdir -p /tmp/unsplash
+# MONITORS=$(polybar --list-monitors | cut -d":" -f1)
 
-if type "xrandr"; then
-  for monitor in $(polybar --list-monitors | cut -d":" -f1); do 
-	  RESOLUTION=$(polybar --list-monitors | grep $monitor | awk '{print $2}' | cut -f1 -d"+")
-	  IMAGE="/tmp/unsplash/$monitor$IMAGE_FORMAT"
-	  UNSPLASH="https://source.unsplash.com/random/${RESOLUTION}/?${PARAMETERS}"
-	  proxychains -q wget --quiet $UNSPLASH_URL -4 -O $IMAGE
-	  # $BACKGROUND_COMMAND $IMAGE
-  done
+# if type "xrandr"; then
+#   for monitor in $(polybar --list-monitors | cut -d":" -f1); do 
+# 	  RESOLUTION=$(polybar --list-monitors | grep $monitor | awk '{print $2}' | cut -f1 -d"+")
+# 	  IMAGE="/tmp/unsplash/$monitor$IMAGE_FORMAT"
+# 	  UNSPLASH="https://source.unsplash.com/random/${RESOLUTION}/?${PARAMETERS}"
+# 	  echo $UNSPLASH $UNSPLASH_URL
+# 	  proxychains -q wget --quiet $UNSPLASH -4 -O $IMAGE
+# 	  # $BACKGROUND_COMMAND $IMAGE
+#   done
 # else
 # if [ $RUN_AS_DAEMON = 1 ]
 # then
@@ -40,9 +41,14 @@ if type "xrandr"; then
 #     $BACKGROUND_COMMAND $IMAGE_SAVE_DIR
 #     sleep $UPDATE_INTERVAL
 #   done &
-else
-    proxychains -q wget --quiet $UNSPLASH_URL -4 -O $IMAGE_SAVE_DIR
-    $BACKGROUND_COMMAND $IMAGE_SAVE_DIR
-fi
+# else
+#     proxychains -q wget --quiet $UNSPLASH_URL -4 -O $IMAGE_SAVE_DIR
+#     $BACKGROUND_COMMAND $IMAGE_SAVE_DIR
+# fi
 
-feh --bg-fill --no-fehbg /tmp/unsplash/HDMI-1-0.jpg /tmp/unsplash/eDP-1.jpg /tmp/unsplash/DP-1-2.jpg
+feh --bg-fill --no-fehbg ~/Pictures/1.jpg ~/Pictures/3.jpg ~/Pictures/2.jpg 
+
+sleep 1
+
+setxkbmap -option
+setxkbmap -option "ctrl:nocaps"
